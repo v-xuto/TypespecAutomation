b55b351794485274f59db7ed7cc353e9900bb51f
v-zhocai <v-zhocai@microsoft.com>
2025-06-19 10:29:44 +0800

    Fixed only throw "failed install extension" on Linux

--- src/common/commonSteps.ts
+++ src/common/commonSteps.ts
@@ -280,17 +280,15 @@ async function installExtensionForCommand(page: Page, extensionDir: string) {
   await screenShot.screenShot("start_install_extension.png")
   await page.keyboard.press("Enter")
   await sleep(8)
-  if (os.platform() === "win32"){
-    await retry(
-      2,
-      async () => {
-        const installed = page.locator('.codicon-terminal-decoration-success')
-        return (await installed.count()) > 0
-      },
-      `Failed to install the extension.`,
-      1
-    )
-  }
+  await retry(
+    2,
+    async () => {
+      const installed = page.getByLabel('Extensions (Ctrl+Shift+X) - 1').nth(2)
+      return (await installed.count()) > 0
+    },
+    `Failed to install the extension.`,
+    1
+  )
 
   await screenShot.screenShot("start_install_extension_result.png")
 }
