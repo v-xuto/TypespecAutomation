bae261e97838130bba252c312ec4c735fc78cad2
v-zhocai <v-zhocai@microsoft.com>
2025-06-19 11:45:13 +0800

    Fixed only throw "failed install extension" on Linux

--- src/common/commonSteps.ts
+++ src/common/commonSteps.ts
@@ -280,16 +280,23 @@ async function installExtensionForCommand(page: Page, extensionDir: string) {
   await screenShot.screenShot("start_install_extension.png")
   await page.keyboard.press("Enter")
   await sleep(8)
+  await page
+    .getByRole("tab", { name: /Extensions/ })
+    .locator("a")
+    .click()
+  await sleep(2)
   await retry(
     2,
     async () => {
-      const installed = page.getByLabel('Extensions (Ctrl+Shift+X) - 1').nth(2)
+      const installed = page.locator('span').filter({ hasText: /^TypeSpec$/ })
       return (await installed.count()) > 0
     },
     `Failed to install the extension.`,
     1
   )
-
+  await page
+    .getByLabel('Explorer').first()
+    .click()  
   await screenShot.screenShot("start_install_extension_result.png")
 }
 
